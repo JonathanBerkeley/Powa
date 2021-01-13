2008e058cb69d0c8009dab5400a78682a149e020
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2021-01-13 15:18:10 +0000

    Constructors added

--- app/src/main/java/com/powapp/powa/data/DataEntity.kt
+++ app/src/main/java/com/powapp/powa/data/DataEntity.kt
@@ -3,12 +3,24 @@ package com.powapp.powa.data
 import com.powapp.powa.NEW_ENTRY_ID
 import java.util.*
 
-data class DataEntity (
-    var id: Int,
-    var date: Date,
-    var text: String
+data class DataEntity(
+        var id: Int,
+        var date: Date,
+        var title: String,
+        var target: String,
+        var target_name: String,
+        var password: String?,
+        var username: String?
 ) {
     //todo encrypt?
-    constructor(): this(NEW_ENTRY_ID, Date(), "")
-    constructor(date: Date, text: String) : this(NEW_ENTRY_ID, date, text)
+    constructor() : this(NEW_ENTRY_ID, Date(), "", "", "", "", "")
+    constructor(date: Date, title: String, target: String, target_name: String, password: String?, username: String?)
+            : this(NEW_ENTRY_ID, date, title, target, target_name, password, username)
+
+    //New constructors
+    /*
+    constructor(id: Int, title: String, target: String, target_name: String
+                , password: String, username: String)
+            : this(NEW_ENTRY_ID, title, target, target_name, password, username)
+    */
 }
