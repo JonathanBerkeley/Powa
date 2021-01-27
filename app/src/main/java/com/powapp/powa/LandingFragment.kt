f260a85f014194247ccfc3a793ed2de19aeb8adb
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2021-01-27 16:04:56 +0000

    Gesture support & navigation backwards

--- app/src/main/java/com/powapp/powa/LandingFragment.kt
+++ app/src/main/java/com/powapp/powa/LandingFragment.kt
@@ -3,11 +3,14 @@ package com.powapp.powa
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.powapp.powa.databinding.LandingFragmentBinding
@@ -23,6 +26,12 @@ class LandingFragment : Fragment(),
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        //Disables back navigation on main screen
+        (activity as AppCompatActivity)
+            .supportActionBar?.setDisplayHomeAsUpEnabled(false)
+
+        //Inflates the fragment and returns reference to binding variable
         binding = LandingFragmentBinding.inflate(inflater, container, false)
         viewModel = ViewModelProvider(this).get(LandingViewModel::class.java)
 
@@ -46,6 +55,10 @@ class LandingFragment : Fragment(),
     //LoginListAdapter interface implementation, handles clicks recycler view clicks
     override fun onItemClick(itemId: Int) {
         Log.i("itemClick", "for id $itemId")
+
+        //Handling ID of item clicked, passing action to nav controller to navigate to the editor
+        val navAction = LandingFragmentDirections.actionEditLogin(itemId)
+        findNavController().navigate(navAction)
     }
 
 }
--- app/src/main/java/com/powapp/powa/ViewLoginFragment.kt
+++ app/src/main/java/com/powapp/powa/ViewLoginFragment.kt
@@ -4,22 +4,68 @@ import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.powapp.powa.databinding.ViewLoginFragmentBinding
 
 class ViewLoginFragment : Fragment() {
     private lateinit var viewModel: ViewLoginViewModel
+    //Lazy evaluation of args passed in from LandingFragment
+    private val args: ViewLoginFragmentArgs by navArgs()
+    private lateinit var binding: ViewLoginFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.view_login_fragment, container, false)
+        //For navigating backwards up the navigation chain - adds back button
+        (activity as AppCompatActivity).supportActionBar?.let {
+            it.setHomeButtonEnabled(true)
+            it.setDisplayShowHomeEnabled(true)
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_back_arrow)
+        }
+        //Gives the icon an options menu
+        setHasOptionsMenu(true)
+
+        //Inflates the fragment with the data passed in from the LandingFragment
+        binding = ViewLoginFragmentBinding.inflate(inflater, container, false)
+        binding.testText.text = "${args.loginId}"
+
+        //Uses the same backward navigation for the back button or hand gestures, so that navigation back with
+        //the SystemUI is handled the same way
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    saveAndNavigateBack()
+                }
+            }
+        )
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ViewLoginViewModel::class.java)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> saveAndNavigateBack()
+            else -> super.onOptionsItemSelected(item)
+        }
+
+    }
+
+    private fun saveAndNavigateBack(): Boolean {
+        findNavController().navigateUp()
+        return true
     }
 
 }
